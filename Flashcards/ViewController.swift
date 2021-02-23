fcca91d5eea34a007dc8c3869a67e57f3d540d49
Sydnee <stanxy357@gmail.com>
2021-02-23 13:37:34 -0800

    Tried to make corners and shadows but only one shows up

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.3
+        
     }
 
-    @IBOutlet weak var frontLabel: UILabel!
-    @IBOutlet weak var backLabel: UILabel!
+
     
     @IBAction func didTapOnFlashCard(_ sender: Any) {
         frontLabel.isHidden = true
